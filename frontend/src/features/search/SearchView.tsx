5d5df218b3f837692afce7126431d5b803adadce
Shalom Prinz <shalom.prinz@gmail.com>
2026-08-17 22:19:58 +0300

    css: co-locate the Search page section beside its components

--- frontend/src/features/search/SearchView.tsx
+++ frontend/src/features/search/SearchView.tsx
@@ -5,6 +5,7 @@ import { useCourseSummaries } from './hooks/useCourseSummaries'
 import { buildHit, findMatches, groupMatches, type Hit, type MatchGroup } from './utils/search'
 import SearchResult from './components/SearchResult'
 import '@/styles/panel.css'
+import './SearchView.css'
 
 const COURSE_STORAGE_KEY = 'fastStudySearchCourse'
 
